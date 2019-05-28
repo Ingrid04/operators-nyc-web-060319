e381bafd8a3b220de37f18537848a1374d1b9fe6
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-28 02:12:42 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
+speed = 50
+
 def unsafe?(speed)
-
+  if speed < 40
+    print "true"
+  elsif speed > 60
+    print "true"
+  else
+    print "false"
 end
+unsafe?
 
-
+speed = 50
 
 def not_safe?(speed)
-	
+	 40 < speed > 60? "true" : "false"
 end
-	
-
-
+not_safe?
